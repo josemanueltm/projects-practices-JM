8d127966c449387a30865a6a9d351e07234df6fb
Jose Manuel Torres Mena <josemanuelsmryoni@gmail.com>
2025-05-16 12:44:18 +0200

    add projects

--- JM-my-portfolio/src/components/Projects.vue
+++ JM-my-portfolio/src/components/Projects.vue
@@ -4,17 +4,18 @@
     <div class="projects-container">
       <div class="project-card">
         <h4 class="project-title">{{ $t('projects.title') }}</h4>
-        <!-- <img src="#" alt="Vista Previa de To-Do-List" class="project-image">
+        <img src="/public/img/To-do-list.png" alt="Vista Previa de To-Do-List" class="project-image">
         <p class="project-description"> Aplicación web desarrollada con Vue.js y Node.js para gestionar tareas en tiempo
           real.</p>
         <div class="project-links">
-          <a href="#" target="_blank" aria-label="GitHub">
+          <a href="https://github.com/josemanueltm/projects-practices-JM/tree/main/JM-TodoList" target="_blank"
+            aria-label="GitHub">
             <i class="fab fa-github"></i>
           </a>
-          <a href="#" target="_blank" aria-label="Demo en Vercel">
-            <img src="#" alt="Demo Vercel" class="vercel-icon" />
+          <a href="https://jm-todolist.vercel.app/" target="_blank" aria-label="Demo en Vercel">
+            <img src="/public/img/vercel.png" alt="Demo Vercel" class="vercel-icon" />
           </a>
-        </div> -->
+        </div>
       </div>
     </div>
   </section>
