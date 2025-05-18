bfa299061ee789d36ba6c42d3286b16b08d845df
Jose Manuel Torres Mena <josemanuelsmryoni@gmail.com>
2025-05-18 09:49:29 +0200

    update

---
+++ WeatherApp/src/components/WeatherForm.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { ref } from 'vue'
+
+const emit = defineEmits(['search-weather'])
+const city = ref('')
+
+function handleSubmit() {
+  emit('search-weather', city.value)
+  city.value = ''
+}
+
+</script>
+
+<template>
+  <form @submit.prevent="handleSubmit" class="form">
+    <input v-model="city" type="text" placeholder="Ej. Madrid" required />
+    <button type="submit">Buscar</button>
+  </form>
+
+</template>
+
+<style scoped>
+.form {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 1.5rem;
+}
+
+input[type="text"] {
+  padding: 0.5rem;
+  border-radius: 8px 0 0 8px;
+  border: 1px solid #ccc;
+  flex: 1;
+  font-size: 1rem;
+}
+
+button {
+  padding: 0.5rem 1rem;
+  border: none;
+  background-color: #0077ff;
+  color: white;
+  border-radius: 0 8px 8px 0;
+  font-size: 1rem;
+  cursor: pointer;
+  transition: background 0.3s;
+}
+
+button:hover {
+  background-color: #005fcc;
+}
+</style>
