2f00e0260636ed7efb1b176751070acde1528616
Jose Manuel Torres Mena <josemanuelsmryoni@gmail.com>
2025-05-19 11:37:30 +0200

    Update

--- JM-my-portfolio/src/components/Projects.vue
+++ JM-my-portfolio/src/components/Projects.vue
@@ -2,8 +2,9 @@
   <section id="projects">
     <h3 class="title-project">{{ $t('projects.titles') }}</h3>
     <div class="projects-container">
+      <!-- Card 1-->
       <div class="project-card">
-        <h4 class="project-title">{{ $t('projects.title') }}</h4>
+        <h4 class="project-title">To-Do-List</h4>
         <img src="/public/img/To-do-list.png" alt="Vista Previa de To-Do-List" class="project-image">
         <p class="project-description"> Aplicación web desarrollada con Vue.js y Node.js para gestionar tareas en tiempo
           real.</p>
@@ -13,6 +14,18 @@
             <i class="fab fa-github"></i>
           </a>
           <a href="https://jm-todolist.vercel.app/" target="_blank" aria-label="Demo en Vercel">
+            <p class="vercel-demo">Demo</p>
+          </a>
+        </div>
+      </div>
+      <!-- Card 2 -->
+      <div class="project-card">
+        <h4 class="project-title">Weather APP</h4>
+        <img src="/public/img/weather.png" alt="Vista Previa de Weather APP" class="project-image">
+        <p class="project-description"> Aplicación web desarrollada con Vue.js y Node.js para saber el tiempo
+          de tu localidad</p>
+        <div class="project-links">
+          <a href="https://mi-primer-repo-five.vercel.app/" target="_blank" aria-label="Demo en Vercel">
             <p class="vercel-demo">Demo</p>
           </a>
         </div>
