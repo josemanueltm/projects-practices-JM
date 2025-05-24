0c6e641ba3a11dea44fe4aa360a6aa84866b8708
Jose Manuel Torres Mena <josemanuelsmryoni@gmail.com>
2025-05-24 12:20:09 +0200

    New project

--- JM-my-portfolio/src/components/Projects.vue
+++ JM-my-portfolio/src/components/Projects.vue
@@ -20,12 +20,27 @@
       </div>
       <!-- Card 2 -->
       <div class="project-card">
-        <h4 class="project-title">Weather APP</h4>
+        <h4 class="project-title">Weather-app</h4>
         <img src="/public/img/weather.png" alt="Vista Previa de Weather APP" class="project-image">
         <p class="project-description"> Aplicación web desarrollada con Vue.js y Node.js para saber el tiempo
           de tu localidad</p>
         <div class="project-links">
           <a href="https://mi-primer-repo-five.vercel.app/" target="_blank" aria-label="Demo en Vercel">
+            <p class="vercel-demo">Demo</p>
+          </a>
+        </div>
+      </div>
+      <!-- Card 3 -->
+      <div class="project-card">
+        <h4 class="project-title">Notes-App</h4>
+        <img src="/public/img/weather.png" alt="Vista Previa de Weather APP" class="project-image">
+        <p class="project-description"> Aplicación web desarrollada con Vue.js Router y Node.js para escribir notas</p>
+        <div class="project-links">
+          <a href="https://github.com/josemanueltm/projects-practices-JM/tree/main/notes-app" target="_blank"
+            aria-label="GitHub">
+            <i class="fab fa-github"></i>
+          </a>
+          <a href="https://notes-app-sigma-neon.vercel.app/" target="_blank" aria-label="Demo en Vercel">
             <p class="vercel-demo">Demo</p>
           </a>
         </div>
